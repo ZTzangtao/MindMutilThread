dab2827960889bd4a30cb0fb247043d5af505694
zt <906577196@qq.com>
2019-10-07 13:29:20 +0800

    修改

--- src/java/ThreadMethodDemo20191007/ThreadSleepDemo.java
+++ src/java/ThreadMethodDemo20191007/ThreadSleepDemo.java
@@ -8,15 +8,18 @@ package ThreadMethodDemo20191007;
  */
 public class ThreadSleepDemo {
     public static void main(String[] args) {
-        new Thread(()->{
-           for (int x = 0;x<10;x++){
-               System.out.println(Thread.currentThread().getName() + "、x=" + x);
-               try {
-                   Thread.sleep(100);
-               } catch (InterruptedException e) {
-                   e.printStackTrace();
-               }
-           }
-        },"线程对象").start();
+        for(int num = 0; num <5; num++){
+            new Thread(()->{
+                for (int x = 0;x<10;x++){
+                    System.out.println(Thread.currentThread().getName() + "、x=" + x);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },"线程对象-" + num).start();
+        }
+
     }
 }
