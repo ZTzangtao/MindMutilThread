1901ba4c854d69759e913d294943960c30671e20
zangtao <906577196@qq.com>
2019-10-26 16:32:52 +0800

    修改

--- src/java/ThreadSimpleDemo20191006/ThreadDemoTicket02.java
+++ src/java/ThreadSimpleDemo20191006/ThreadDemoTicket02.java
@@ -10,19 +10,28 @@ public class ThreadDemoTicket02 {
 
     public static void main(String[] args) {
         TicketThread ticketThread = new TicketThread();
-        new Thread(ticketThread).start();
-        new Thread(ticketThread).start();
-        new Thread(ticketThread).start();
+        new Thread(ticketThread,"票贩子A").start();
+        new Thread(ticketThread,"票贩子B").start();
+        new Thread(ticketThread,"票贩子C").start();
     }
 }
 class TicketThread implements Runnable{
-    private Integer ticket = 5;
+    /**
+     * 直接内存操作，没有中间的拷贝与复制操作
+     */
+    private volatile Integer ticket = 50000;
     @Override
     public void run() {
-        for(int x = 0; x<100;x++){
-            if (ticket > 0){
-                System.out.println("卖票,ticket = " + this.ticket --);
+        synchronized (this){
+            while (this.ticket > 0){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+                System.out.println(Thread.currentThread().getName()
+                        + "卖票处理，ticket= "+this.ticket--);
             }
+        }
     }
 }
